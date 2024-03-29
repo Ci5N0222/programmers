ff407daa15c84a54a67e93a7e069dfeac4462b3d
SionNoh <vmfpsel@gmail.com>
2024-03-29 23:33:11 +0900

    day1 setup

--- js/day1.js
+++ js/day1.js
@@ -2,17 +2,14 @@
 const test1 = (str) => {
     console.log(str);
 }
-// test1('hi');
 
 
 // 정수 a와 b가 주어집니다. 
 // 각 수를 입력받아 입출력 예와 같은 형식으로 출력하는 코드를 작성해 보세요.
-// 입력 : 4 5
 // 출력 : a = 4, b = 5
 const test2 = (a, b) => {
     console.log(`a = ${a}\nb = ${b}`);
 }
-// test2(1, 2);
 
 
 // 문자열 str과 정수 n이 주어집니다.
@@ -20,4 +17,13 @@ const test2 = (a, b) => {
 const test3 = (str, n) => {
     console.log(str.repeat(n));
 }
-// test3('string', 3);
+
+
+// 영어 알파벳으로 이루어진 문자열 str이 주어집니다. 
+// 각 알파벳을 대문자는 소문자로 소문자는 대문자로 변환해서 출력하는 코드를 작성해 보세요.
+const test4 = (str) => {
+    const result = Array.from(str, index => {
+        return index === index.toUpperCase() ? index.toLowerCase() : index.toUpperCase()
+    }).join("");
+    console.log(result);
+}
