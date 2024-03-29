1070ff4bc19ed14755f0f94afc128fdff32a1aeb
SionNoh <vmfpsel@gmail.com>
2024-03-29 23:18:27 +0900

    practice 1day

---
+++ js/day1.js
@@ -0,0 +1,23 @@
+// 문자열 str이 주어질 때, str을 출력하는 코드를 작성해 보세요
+const test1 = (str) => {
+    console.log(str);
+}
+// test1('hi');
+
+
+// 정수 a와 b가 주어집니다. 
+// 각 수를 입력받아 입출력 예와 같은 형식으로 출력하는 코드를 작성해 보세요.
+// 입력 : 4 5
+// 출력 : a = 4, b = 5
+const test2 = (a, b) => {
+    console.log(`a = ${a}\nb = ${b}`);
+}
+// test2(1, 2);
+
+
+// 문자열 str과 정수 n이 주어집니다.
+// str이 n번 반복된 문자열을 만들어 출력하는 코드를 작성해 보세요.
+const test3 = (str, n) => {
+    console.log(str.repeat(n));
+}
+// test3('string', 3);
