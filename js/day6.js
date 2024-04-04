f6c31af8ec58a6fec66cddd465789760e9609e7a
SionNoh <vmfpsel@gmail.com>
2024-04-05 00:41:02 +0900

    day6 setup

---
+++ js/day6.js
@@ -0,0 +1,13 @@
+// boolean 변수 x1, x2, x3, x4가 매개변수로 주어질 때, 
+// 다음의 식의 true/false를 return 하는 solution 함수를 작성해 주세요.
+// (x1 ∨ x2) ∧ (x3 ∨ x4)
+// ∨ : or
+// ∧ : and
+const test1 = (x1, x2, x3, x4) => {
+    let answer = true;
+    if(!x1 && !x2) answer = false;
+    if(!x3 && !x4) answer = false;
+    return answer;
+}
+
+
