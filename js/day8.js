11d3071744cf4c107eaac9d15c139b0633db85c8
C-ON <vmfpsel@gmail.com>
2024-04-06 23:26:07 +0900

    day 8 clear

--- js/day8.js
+++ js/day8.js
@@ -34,3 +34,13 @@ const test4 = (n_str) => {
     answer = parseInt(n_str);
     return answer;
 }
+
+
+// 정수 배열 arr와 자연수 k가 주어집니다.
+// 만약 k가 홀수라면 arr의 모든 원소에 k를 곱하고, k가 짝수라면 arr의 모든 원소에 k를 더합니다.
+// 이러한 변환을 마친 후의 arr를 return 하는 solution 함수를 완성해 주세요.
+const test5 = (arr, k) => {
+    let answer = [];
+    answer = k%2 === 0 ? answer = arr.map(item => item + k) : answer = arr.map(item => item * k)
+    return answer;
+}
