c0b3c42a527830bcfd3ad0148390976de233499a
Sion-Noh <vmfpsel@gmail.com>
2024-04-18 21:04:20 +0900

    day 21 clear

--- js/level-0/day21.js
+++ js/level-0/day21.js
@@ -47,3 +47,12 @@ const test4 = (array) => {
     let arr = array.sort((a, b) => a - b);
     return arr[Math.floor(array.length/2)];
 }
+
+
+// 정수 배열 array가 매개변수로 주어질 때, 
+// 가장 큰 수와 그 수의 인덱스를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+const test5 = (array) => {
+    const max = Math.max(...array); 
+    const index = array.indexOf(max); 
+    return [max, index];
+}
